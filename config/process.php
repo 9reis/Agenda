d55960f680eb0490d9010f87108a332448b3be62
Lucas Reis <lucasreis_9@hotmail.com>
2022-08-08 11:18:10 -0300

    Codigo que resgata um unico contato do back end e mostra na view

--- config/process.php
+++ config/process.php
@@ -5,12 +5,36 @@
     include_once('connection.php');
     include_once('url.php');
 
-    $contacts = [ ];
+    $id; 
 
-    $query = "SELECT * FROM contacts";
+    if(!empty($_GET)){
+        $id = $_GET['id'];
+    }
+    // RETORNA OS DADOS DE UM CONTATO
+    if(!empty($id)){
 
-    $stmt = $conn->prepare($query);
+        $query = "SELECT * FROM contacts WHERE id= :id";
 
-    $stmt -> execute();
+        $stmt = $conn->prepare($query);
 
-    $contacts = $stmt->fetchAll();
+        $stmt->bindParam(":id",$id);
+
+        $stmt->execute();
+
+        $contact = $stmt-> fetch();
+
+    }else{
+        //RETORNA TODOS OS CONTATOS
+        $contacts = [ ];
+
+        $query = "SELECT * FROM contacts";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt -> execute();
+
+        $contacts = $stmt->fetchAll();
+    }
+
+    
+    
--- index.php
+++ index.php
@@ -28,7 +28,7 @@ include_once('templates/header.php')
                             <td scope="row"><?= $contact['name'] ?></td>
                             <td scope="row"><?= $contact['phone'] ?></td>
                             <td class="actions">
-                                <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                                <a href="<?= $BASE_URL ?>show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
                                 <a href="#"><i class="far fa-edit edit-icon"></i></a>
                                 <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
                             </td>
