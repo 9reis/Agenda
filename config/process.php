194e8302be363b46bc7ce5b2260dce969d0e9aec
Lucas Reis <lucasreis_9@hotmail.com>
2022-08-10 20:16:40 -0300

    Finalizada a logica para editar o contato (UPDATE)

--- config/process.php
+++ config/process.php
@@ -34,13 +34,38 @@
         
             };
 
-        };
+        }else if($data["type"] === "edit") {
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+    
+            $query = "UPDATE contacts SET name = :name, phone = :phone, observations = :observations 
+                    WHERE id = :id";
+            
+            $stmt = $conn-> prepare($query);
+    
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+    
+            try{
+                $stmt->execute();
+                $_SESSION["msg"] = "Contato atualizado com sucesso!";
+            }catch(PDOException $e){
+                //Erro na conexão
+                $error = $e -> getMessage();
+                echo "Erro: $error";
+            };
+        }
         
         //redirect HOME
-        header("Location:" . $BASE_URL . "../index.php");
+         header("Location:" . $BASE_URL . "../index.php");
 
+    }
     //SELEÇÃO DE DADOS 
-    }else {
+    else {
         
         $id; 
 
